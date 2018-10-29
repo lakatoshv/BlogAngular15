4ae32e7e145f17c228aa741bc323cb7dc3911db3
lakatoshvv <some.pochta1@gmail.com>
2018-10-29 23:32:30 +0200

    created component posts-list in shared/posts

--- src/app/shared/posts/posts.module.ts
+++ src/app/shared/posts/posts.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PostsRoutingModule
   ],
-  declarations: []
+  declarations: [PostsListComponent]
 })
 export class PostsModule { }
