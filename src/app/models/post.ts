19099539b7b19276f46cd389cbe18465aa61ad19
lakatoshvv <some.pochta1@gmail.com>
2018-10-30 17:05:25 +0200

    created post model in models folder

---
+++ src/app/models/post.ts
@@ -0,0 +1,25 @@
+export class Post {
+    /*
+    id -> int, increment
+    title -> string
+    description -> string/text
+    content -> string/text
+    author  -> string/text
+    seen -> int
+    likes -> int
+    dislikes -> int
+    tags -> string
+    imgurl -> string
+    //access -> string
+    */
+    id: number;
+    title: string;
+    description: string;
+    content: string;
+    author: string;
+    seen: number;
+    likes: number;
+    dislikes: number;
+    tags: number;
+    imgurl: string;
+  }
