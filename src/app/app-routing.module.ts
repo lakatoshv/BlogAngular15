0bcb0837b4a2e570dadd83568c322e0cea38eb36
lakatoshvv <some.pochta1@gmail.com>
2018-10-31 12:44:51 +0200

    added default redirect to user-portal module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: "./user-portal/user-portal.module#UserPortalModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 
+import { PostsListComponent } from "./posts/posts-list/posts-list.component"
+
 @NgModule({
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  declarations: []
+  declarations: [
+    PostsListComponent
+  ],
+  exports: [
+    PostsListComponent
+  ]
 })
 export class SharedModule { }
