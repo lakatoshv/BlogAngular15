6cc039a8b664d7f8e731c22e34126db70bdb4d44
lakatoshvv <some.pochta1@gmail.com>
2018-11-05 20:17:27 +0200

    Added layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-portal/layout.module.ts
+++ src/app/user-portal/layout.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import {LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponentComponent } from './layout-component/layout-component.component';
 import { SharedModule } from '../shared';
-
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 // import { SharedModule } from "@app/shared";
 
 @NgModule({
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    AngularFontAwesomeModule
   ],
   declarations: [LayoutComponentComponent]
 })
