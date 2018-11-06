8399edf8d6c0af7947bf81c79af8ed853ec37212
lakatoshvv <some.pochta1@gmail.com>
2018-11-06 23:56:17 +0200

    Added show component to posts module

--- src/app/shared/posts/posts-routing.module.ts
+++ src/app/shared/posts/posts-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { ShowComponent } from './show/show.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: PostsListComponent
+    component: PostsListComponent,
+    children: [
+      {
+        path: "post/:post-id",
+        loadChildren: './show/show.component'
+      }
+    ]
   }
 ];
 
--- src/app/shared/posts/posts.module.ts
+++ src/app/shared/posts/posts.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { ShowComponent } from './show/show.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PostsRoutingModule
   ],
-  declarations: [PostsListComponent]
+  declarations: [PostsListComponent, ShowComponent]
 })
 export class PostsModule { }
