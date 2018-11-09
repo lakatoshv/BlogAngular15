b6d8fbb2c75acaaaa3dcbad7647577ca1a2c8529
lakatoshvv <some.pochta1@gmail.com>
2018-11-09 16:05:22 +0200

    Created model and datafile for commients

---
+++ src/app/core/data/comments.ts
@@ -0,0 +1,46 @@
+import { Comment } from '../models/comment';
+
+export const Comments: Comment[] = [
+    {
+        id: 0,
+        post_id: 4,
+        author: "Katy Liu",
+        content: "Pick the yellow peach that looks like a sunset with its red, orange, and pink coat skin, peel it off with your teeth. Sink them into unripened.",
+        created_at: new Date()
+    },
+    {
+        id: 0,
+        post_id: 4,
+        author: "Katy Liu",
+        content: "Pick the yellow peach that looks like a sunset with its red, orange, and pink coat skin, peel it off with your teeth. Sink them into unripened.",
+        created_at: new Date()
+    },
+    {
+        id: 0,
+        post_id: 4,
+        author: "Katy Liu",
+        content: "Pick the yellow peach that looks like a sunset with its red, orange, and pink coat skin, peel it off with your teeth. Sink them into unripened.",
+        created_at: new Date()
+    },
+    {
+        id: 1,
+        post_id: 4,
+        author: "Katy Liu",
+        content: "Pick the yellow peach that looks like a sunset with its red, orange, and pink coat skin, peel it off with your teeth. Sink them into unripened.",
+        created_at: new Date()
+    },
+    {
+        id: 1,
+        post_id: 4,
+        author: "Katy Liu",
+        content: "Pick the yellow peach that looks like a sunset with its red, orange, and pink coat skin, peel it off with your teeth. Sink them into unripened.",
+        created_at: new Date()
+    },
+    {
+        id: 0,
+        post_id: 4,
+        author: "Katy Liu",
+        content: "Pick the yellow peach that looks like a sunset with its red, orange, and pink coat skin, peel it off with your teeth. Sink them into unripened.",
+        created_at: new Date()
+    }
+];
---
+++ src/app/core/models/comment.ts
@@ -0,0 +1,14 @@
+export class Comment {
+    /*
+    id -> int, increment
+    post_id -> int
+    author -> string
+    content -> text
+    created_at -> date
+    */
+    id: number;
+    post_id: number;
+    author: string;
+    content: string;
+    created_at: Date;
+  }
