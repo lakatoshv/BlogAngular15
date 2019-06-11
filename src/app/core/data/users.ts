8f54f2b9cf1f349f25fb21a66cc999214e78372f
Віталій Лакатош <some.pochta1@gmail.com>
2019-06-11 13:58:57 +0300

    Authorize user

---
+++ src/app/core/data/users.ts
@@ -0,0 +1,54 @@
+import { User } from '../models/user';
+
+export const Users: User[] = [
+    { 
+        Id: 0,
+        UserName: "test1@test.test",
+        Email: "test1@test.test",
+        FirstName: "test",
+        LastName: "test",
+        PhoneNumber: "0123456789",
+        Roles: ["User"],
+        Password: "123123"
+    },
+    { 
+        Id: 1,
+        UserName: "test2@test.test",
+        Email: "test2@test.test",
+        FirstName: "test",
+        LastName: "test",
+        PhoneNumber: "0123456789",
+        Roles: ["User"],
+        Password: "123123"
+    },
+    { 
+        Id: 2,
+        UserName: "test3@test.test",
+        Email: "test3@test.test",
+        FirstName: "test",
+        LastName: "test",
+        PhoneNumber: "0123456789",
+        Roles: ["User"],
+        Password: "123123"
+    },
+    { 
+        Id: 3,
+        UserName: "test4@test.test",
+        Email: "test4@test.test",
+        FirstName: "test",
+        LastName: "test",
+        PhoneNumber: "0123456789",
+        Roles: ["User"],
+        Password: "123123"
+    },
+    { 
+        Id: 4,
+        UserName: "test5@test.test",
+        Email: "test5@test.test",
+        FirstName: "test",
+        LastName: "test",
+        PhoneNumber: "0123456789",
+        Roles: ["User"],
+        Password: "123123"
+    }
+];
--- src/app/user-portal/user/authorization/authorization.component.ts
+++ src/app/user-portal/user/authorization/authorization.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AuthorizationForm } from 'src/app/core/forms/user/AuthorizationForm';
 import { Router } from '@angular/router';
+import { UsersService } from 'src/app/core/services/users/users-service.service';
+import { JwtToken } from 'src/app/core/models/JwtToken';
 
 @Component({
   selector: 'app-authorization',
@@ -12,38 +14,31 @@ export class AuthorizationComponent implements OnInit {
   private _authorizationForm: FormGroup = new AuthorizationForm().authorizationForm;
   
   constructor(
-    private _router: Router
+    private _router: Router,
+    private _usersService: UsersService
   ) { }
 
   ngOnInit() {
   }
 
   private _authorization(dataForAuthorize){
-    /*
     if (this._authorizationForm.valid) {
-      this._usersService.login(dataForAuthorize)
-        .subscribe(
-          (jwt:JwtToken) => {
-            if (jwt) {
-              this.succesLogin(jwt);
-            }
-          },
-          (errorMessage) => {}
-        );
-    }*/
+      var user = this._usersService.login(dataForAuthorize);
+      if(user)
+        this.succesLogin(user)
+    }
   }
 
-  public succesLogin(): void {
+  public succesLogin(user: string): void {
+    this._usersService.saveUser(user);
     /*
-    this._usersService.saveToken(jwt["auth_token"], jwt["refresh_token"]);
     const initializeSubscription = this._accountService.initialize(this._globalService._currentUser.Id).subscribe(
         (initializationData) => {
             this._globalService.initializeData(initializationData);
+            this._router.navigate(['/']);
         },
         (errorMessage) => {}
-    );*/
-    //this._subscription.add(initializeSubscription);
-    
-    //this._globalService.setIsLoadedData(false);
-  }
+    );
+    */
+}
 }
