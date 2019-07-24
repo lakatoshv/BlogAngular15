88f760016fa6df77b8b9901af4d1a7b5e515cd62
Vitaliy Lakatosh <vlakatosh@netfully.com>
2019-07-24 16:51:53 +0300

    Only author can edit own comment

--- src/app/shared/posts/comments/edit-comment/edit-comment.component.ts
+++ src/app/shared/posts/comments/edit-comment/edit-comment.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { CommentForm } from 'src/app/core/forms/posts/CommentForm';
 
 import { Comment } from "../../../../core/models/comment";
+import { User } from 'src/app/core/models/user';
+import { UsersService } from 'src/app/core/services/users/users-service.service';
+import { GlobalService } from 'src/app/core/services/global-service/global-service.service';
 
 @Component({
   selector: 'app-edit-comment',
@@ -11,15 +14,28 @@ import { Comment } from "../../../../core/models/comment";
 })
 export class EditCommentComponent implements OnInit {
   @Output() onEdit = new EventEmitter<any>();
-  @Input() comment: any;
+  @Input() comment: Comment;
+
+  public loggedIn: boolean = false;
+  public user: User = null;
 
   private _commentForm: FormGroup = new CommentForm().commentForm;
 
-  constructor() { 
+  constructor(
+    private _usersService: UsersService,
+    private _globalService: GlobalService
+  ) { 
   }
 
   ngOnInit() {
-    this.setFormValue();
+    this.loggedIn = this._usersService.isLoggedIn();
+
+    if(this.loggedIn){
+      this._globalService.resetUserData();  
+      this.user = this._globalService._currentUser;
+    }
+    if(this.user.Id === this.comment.authorId)
+      this.setFormValue();
   }
 
   public setFormValue(){
