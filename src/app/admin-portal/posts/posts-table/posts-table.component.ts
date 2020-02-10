c27a28a6b13801c796464f8d9780824cfabad906
Віталій Лакатош <some.pochta1@gmail.com>
2020-02-10 19:04:40 +0200

    Create posts module

---
+++ src/app/admin-portal/posts/posts-table/posts-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-table',
+  templateUrl: './posts-table.component.html',
+  styleUrls: ['./posts-table.component.css']
+})
+export class PostsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
